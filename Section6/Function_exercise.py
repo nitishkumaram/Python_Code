cad7ac0c512dcd26cf49dd406de5be973c9b8c5a
nitishkumaram <nitishkumar@outlook.com>
2020-05-10 10:15:09 +0530

    join and abs

--- Section6/Function_exercise.py
+++ Section6/Function_exercise.py
@@ -163,3 +163,20 @@ def master_yoda(text):
 master_yoda('I am home')
 # Check
 master_yoda('We are ready')
+
+# ALMOST THERE: Given an integer n, return True if n is within 10 of either 100 or 200
+# almost_there(90) --> True
+# almost_there(104) --> True
+# almost_there(150) --> False
+# almost_there(209) --> True
+# NOTE: abs(num) returns the absolute value of a number
+
+def almost_there(n):
+    return (abs(100-n) <= 10) or (abs(200-n) <=10)
+
+# Check
+almost_there(104)
+# Check
+almost_there(150)
+# Check
+almost_there(209)
